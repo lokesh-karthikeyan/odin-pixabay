e41eb7591699ffc0a871c3549adc46f3444eb606
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-22 23:18:54 +0530

    API: Add `#fetch_image_urls` to process "HTTP response"

--- app/services/pixabay_service.rb
+++ app/services/pixabay_service.rb
@@ -4,10 +4,19 @@ class PixabayService
   PER_PAGE = 10
 
   def pixabay_url_builder(query_string)
+    return if query_string.nil? || query_string.blank?
+
     url = "#{BASE_URL}?key=#{Rails.application.credentials.pixabay.api_key}&q=#{query_string}&image_type=#{IMAGE_TYPE}&per_page=#{PER_PAGE}"
     response = RestClient.get(url)
     JSON.parse(response.body)
   rescue RestClient::ExceptionWithResponse => e
     { "error" => e.response }
+  end
+
+  def fetch_image_urls(url)
+    return if url.nil?
+
+    results = url["hits"]
+    results.map { |result| result["largeImageURL"] }
   end
 end
