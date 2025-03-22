de582ffe33642b1d5ede5847eefee8a70e6b35e5
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-22 23:17:42 +0530

    Controller: Add `#pixabay_search` action controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,8 @@
 class StaticPagesController < ApplicationController
+  def pixabay_search
+    service = PixabayService.new
+    @search_content = params[:query_string]
+    http_response = service.pixabay_url_builder(@search_content)
+    @images = service.fetch_image_urls(http_response)
+  end
 end
